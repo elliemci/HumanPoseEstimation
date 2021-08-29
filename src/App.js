9d807a5e3c83754830f0868d25d05920f99d667a
Ellie McIntosh <e_paskaleva@hotmail.com>
2021-08-28 22:06:10 -0700

    Adding and training tensorflow.js model

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import React, { useState } from 'react'
+import React from 'react'
 import * as tf from '@tensorflow/tfjs'
 import logo from './fitness_logo.png'
 import './App.css'
 
-const handleRunTraining = (event) => {
-  // console.log('Run training');
-  tf.tensor([[1, 2, 3], [4, 5, 6], [2, 3]]).print();
-}
 
-function WelcomeMessage({ children }){
-  return <p>{children}</p>
+const handleRunTraining = (event) => {
+
+  // define a one-layer sequential model handled by the listener
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+   // compile the model with SGD optimizer and meanSquareError loss
+   model.compile({ optimizer: tf.train.sgd(0.1), loss: 'meanSquaredError' });
+   model.summary();
+
+   // define input data based on the equation y = 2x - 1
+   const xs = tf.tensor2d([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [6, 1]);
+   const ys = tf.tensor2d([-3.0, -1.0, 2.0, 3.0, 5.0, 7.0], [6, 1]);
+
+   // pass the input and terget attributes into the training function;
+   // when doTraining call is done, predict function is called on 10
+   doTraining(model, xs, ys).then(() => {
+     // call the model to predict for unseeen data point
+     const prediction = model.predict(tf.tensor2d([10], [1, 1]));
+     var res = prediction.dataSync()[0];
+     prediction.dispose();
+     // print the prediction result
+     console.log('Result: ' + res);
+   });
+ };
+
+function WelcomeMessage({ children }) {
+    return <p>{children}</p>
+    } 
+
+async function doTraining(model, xs, ys) {
+  const history =
+    await model.fit(xs, ys,
+      {
+        epochs: 200,
+        callbacks: {
+          onEpochEnd: async (epoch, logs) => {
+            // log the training statistics
+            console.log("Epoch:"
+              + epoch
+              + " Loss:"
+              + logs.loss);
+
+          }
+        }
+      });
+  console.log(history.params);
 }
 
 function App() {
@@ -28,7 +69,6 @@ function App() {
         </a>
         <br />
         <button onClick={handleRunTraining} > Run training </button>
-
       </header>
     </div>
   );
