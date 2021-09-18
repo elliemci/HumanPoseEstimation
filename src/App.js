45adb0a9ff0dd1e782fb80c7ecbcbc0202bde288
Ellie McIntosh <e_paskaleva@hotmail.com>
2021-09-18 14:11:49 -0700

    Adding Webcam to the app

--- src/App.js
+++ src/App.js
@@ -1,65 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import * as tf from '@tensorflow/tfjs'
-import * as posenet from '@tensorflow-models/posenet'
+import React, { useEffect, useState, useRef } from 'react';
+import Webcam from 'react-webcam';
 
-import * as webgl from '@tensorflow/tfjs-backend-webgl'
+import * as tf from '@tensorflow/tfjs';
+import * as posenet from '@tensorflow-models/posenet';
+import * as webgl from '@tensorflow/tfjs-backend-webgl';
+
 import logo from './fitness_logo.png'
 import './App.css'
 import { tSExternalModuleReference } from '@babel/types'
+import { scalePoses } from '@tensorflow-models/posenet/dist/util';
 
-
-/*const handleRunTraining = (event) => {
-
-  // define a one-layer sequential model handled by the listener
-  const model = tf.sequential();
-  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
-
-   // compile the model with SGD optimizer and meanSquareError loss
-  model.compile({ optimizer: tf.train.sgd(0.1), loss: 'meanSquaredError' });
-  model.summary();
-
-   // define input data based on the equation y = 2x - 1
-  const xs = tf.tensor2d([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [6, 1]);
-  const ys = tf.tensor2d([-3.0, -1.0, 2.0, 3.0, 5.0, 7.0], [6, 1]);
-
-   // pass the input and target attributes into the training function;
-   // when doTraining call is done, predict function is called on 10
-  doTraining(model, xs, ys).then(() => {
-    // call the model to predict for unseeen data point
-    const prediction = model.predict(tf.tensor2d([10], [1, 1]));
-    var res = prediction.dataSync()[0];
-    prediction.dispose();
-    // print the prediction result
-    console.log('Result: ' + res);
-   });
- }*/
 
 function WelcomeMessage({ children }) {
   return <p>{children}</p>
   }
 
-/*async function doTraining(model, xs, ys) {
-  const history =
-    await model.fit(xs, ys,
-      {
-        epochs: 200,
-        callbacks: {
-          onEpochEnd: async (epoch, logs) => {
-            // log the training statistics
-            console.log("Epoch:"
-              + epoch
-              + " Loss:"
-              + logs.loss);
-
-          }
-        }
-      });
-  console.log(history.params);
-}*/
 
 function App() {
   /* define a variable model with the useState hook to stores the PoseNet model*/
   const [model, setModel] = useState(null);
+  const webcamRef = useRef(null)
 
    /* tell React what to do when the component is flushed,
     with a function which performs the effect */
@@ -67,9 +27,15 @@ function App() {
     loadPosenet();
   }, [])
 
+  // useRef hook allows a direct reference to the DOM element in the functional component
+  useRef (() => {
+
+  })
+
   /* Load the PoseNet model */
   const loadPosenet = async () => {
-    const posenetModel = await posenet.load({
+    // variables defined with let can be re-assigned
+    let posenetModel = await posenet.load({
       architecture: 'MobileNetV1',
       outputStride: 16,
       inputResolution: { width: 800, height: 600},
@@ -83,7 +49,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
+
+        <Webcam ref={webcamRef} 
+                style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  left: 0,
+                  right: 0,
+                  textAlign: "center",
+                  zindex: 9,
+                  width: 800,  //800
+                  height: 600, //600
+                }}
+        />
+
         <WelcomeMessage children={'Welcome to the Virtual Fitness Assistant!'} />
         <a
           className="App-link"
