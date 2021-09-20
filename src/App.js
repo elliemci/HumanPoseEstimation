dd535c8aba19288fdc652bcc7e69fa006d94516d
Ellie McIntosh <e_paskaleva@hotmail.com>
2021-09-20 12:39:35 -0700

    Adding the PoseNet model with an empty handlePoseEstimation methos with the logic for current state.

--- src/App.js
+++ src/App.js
@@ -11,18 +11,21 @@ import { tSExternalModuleReference } from '@babel/types'
 import { scalePoses } from '@tensorflow-models/posenet/dist/util';
 
 
-function WelcomeMessage({ children }) {
-  return <p>{children}</p>
-  }
-
-
 function App() {
-  /* define a variable model with the useState hook to stores the PoseNet model*/
+  /* define a variable model with the useState hook to stores the PoseNet model */
   const [model, setModel] = useState(null);
-  const webcamRef = useRef(null)
+  // useRef hook stores reference to the DOM element so that interacting with itbypasses the usua React state-to_UI flow
+  const webcamRef = useRef(null);
+  const poseEstimationLoop = useRef(null);
+  // variable with useState hook
+  const [isPoseEstimation, setIsPoseEstimation] = useState(false)
 
-   /* tell React what to do when the component is flushed,
-    with a function which performs the effect */
+
+  function WelcomeMessage({ children }) {
+    return <p>{children}</p>
+    };
+
+   // tell React what to do when the component is flushed, with a function which performs the effect
   useEffect(() => {
     loadPosenet();
   }, [])
@@ -30,7 +33,7 @@ function App() {
   // useRef hook allows a direct reference to the DOM element in the functional component
   useRef (() => {
 
-  })
+  });
 
   /* Load the PoseNet model */
   const loadPosenet = async () => {
@@ -46,13 +49,57 @@ function App() {
     console.log("Posenet Model Loaded!")
   };
 
+  const startPoseEstimation = () => {
+    if ( // check for undefined property
+        typeof webcamRef.current !== "undefined" &&
+               webcamRef.current !== null &&
+               webcamRef.current.video.readyState === 4 
+        ) {
+          // to run the pose estimation define infinite loop executing each 100 miliseconds
+        poseEstimationLoop.current = setInterval(() => {
+         // get video properties from webcamRef
+         const video = webcamRef.current.video;
+         const videoWidth = webcamRef.current.video.videoWidth;
+         const videoHeight = webcamRef.current.video.videoHeight;
+         // set the width and height properties of webcamRef from the video width and height
+         webcamRef.current.video.width = videoWidth;
+         webcamRef.current.video.height = videoHeight;
+         // get the current time
+         var tic = new Date().getTime()
+         // do pose estimation from the loaded PoseNet model, passing the webcamoRef variable
+         model.estimateSinglePose(video, { flipHorizontal : false }).then(pose => {
+          // in the promise of the estimateSinglePose method, log the end time and TensorFlow.js backend and the pose information
+          var toc = new Date().getTime();
+          console.log(toc - tic, " ms");
+          console.log(tf.getBackend());
+          console.log(pose);
+         });
+        }, 100);
+    }
+  };
+
+  const stopPoseEstimation = () => clearInterval(poseEstimationLoop.current);
+
+  const handlePoseEstimation = () => {
+    /* check if pose estimation is running, then stop it, otherwise start pose estimation; 
+       set the setIsPoseEstimation variable to update with current state */
+    if (isPoseEstimation) {
+      stopPoseEstimation();
+    } else {
+      startPoseEstimation();
+    }
+
+    setIsPoseEstimation(current => !current)
+
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        
+       
         <img src={logo} className="App-logo" alt="logo" />
-
-        <Webcam ref={webcamRef} 
+      
+        <Webcam ref={webcamRef} //ref stores the state so that updating the state doesn’t cause the component to re-render.
                 style={{
                   position: "absolute",
                   marginLeft: "auto",
@@ -61,10 +108,26 @@ function App() {
                   right: 0,
                   textAlign: "center",
                   zindex: 9,
-                  width: 800,  //800
-                  height: 600, //600
+                  width: 800,  
+                  height: 600, 
                 }}
         />
+
+        <button style={{
+          height: "50px",
+          width: "100px",
+          position: "relative",
+          marginLeft: "auto",
+          marginRight: "auto",
+          top: 220,
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9}} 
+          onClick = {handlePoseEstimation}>
+          {isPoseEstimation ? "Stop" : "Start"}
+        </button>
+
 
         <WelcomeMessage children={'Welcome to the Virtual Fitness Assistant!'} />
         <a
