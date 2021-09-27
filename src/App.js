1ed89e0f82a19583635b31557ffd56b89ef48f0b
Ellie McIntosh <e_paskaleva@hotmail.com>
2021-09-27 14:49:44 -0700

    Integrated Material-UI library and base UI layout structure

--- src/App.js
+++ src/App.js
@@ -8,37 +8,34 @@ import * as webgl from '@tensorflow/tfjs-backend-webgl';
 
 import logo from './fitness_logo.png'
 import './App.css'
-import { tSExternalModuleReference } from '@babel/types'
+import { tSExternalModuleReference } from '@babel/types';
 import { scalePoses } from '@tensorflow-models/posenet/dist/util';
-
-
+import { Grid } from '@material-ui/core';
 
 function App() {
-  /* define a variable model with the useState hook to stores the PoseNet model */
-  const [model, setModel] = useState(null);
-  // variable with useState hook
-  const [isPoseEstimation, setIsPoseEstimation] = useState(false)
   // useRef hook stores reference to the DOM element so that interacting with itbypasses the usua React state-to_UI flow
   const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  /* define a variable model with the useState hook to stores the PoseNet model */
+  const [model, setModel] = useState(null);
   const poseEstimationLoop = useRef(null);
-  const canvasRef = useRef(null)
-
+  // variable with useState hook
+  const [isPoseEstimation, setIsPoseEstimation] = useState(false);
 
   function WelcomeMessage({ children }) {
     return <p>{children}</p>
     };
 
-   // tell React what to do when the component is flushed, with a function which performs the effect
+  // useRef hook allows a direct reference to the DOM element in the functional component
+  useRef (() => {
+  });
+  
+  // tell React what to do when the component is flushed, with a function which performs the effect  
   useEffect(() => {
     loadPosenet();
   }, [])
 
-  // useRef hook allows a direct reference to the DOM element in the functional component
-  useRef (() => {
-
-  });
-
-  /* Load the PoseNet model */
+  // Load the PoseNet model, which runs on JavaScript API Web Graphics Library WebGL for rendering 2d
   const loadPosenet = async () => {
     // variables defined with let can be re-assigned
     let posenetModel = await posenet.load({
@@ -49,39 +46,39 @@ function App() {
     });
 
     setModel(posenetModel)
-    console.log("Posenet Model Loaded!")
+    console.log("Posenet Model Loaded..")
   };
 
   const startPoseEstimation = () => {
-    if ( // check for undefined property
-        typeof webcamRef.current !== "undefined" &&
-               webcamRef.current !== null &&
-               webcamRef.current.video.readyState === 4 
-        ) {
-          // to run the pose estimation define infinite loop executing each 100 miliseconds
-        poseEstimationLoop.current = setInterval(() => {
-         // get video properties from webcamRef
-         const video = webcamRef.current.video;
-         const videoWidth = webcamRef.current.video.videoWidth;
-         const videoHeight = webcamRef.current.video.videoHeight;
-         // set the width and height properties of webcamRef from the video width and height
-         webcamRef.current.video.width = videoWidth;
-         webcamRef.current.video.height = videoHeight;
-         // get the current time
-         var tic = new Date().getTime()
-         
-         // do pose estimation from the loaded PoseNet model, passing the webcamRef
-         model.estimateSinglePose(video, { flipHorizontal : false }).then(pose => {
+    if (
+      typeof webcamRef.current !== "undefined" && // check for undefined property
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // to run the pose estimation define infinite loop executing each 100 miliseconds
+      poseEstimationLoop.current = setInterval(() => {
+        // get video Properties from webcampRef
+        const video = webcamRef.current.video;
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
+
+        // set the width and height properties of webcamRef from the video width and height
+        webcamRef.current.video.width = videoWidth;
+        webcamRef.current.video.height = videoHeight;
+
+        // get the current time
+        var tic = new Date().getTime()
+        // do pose estimation from the loaded PoseNet model, passing the webcamRef
+        model.estimateSinglePose(video, { flipHorizontal: false }).then(pose => {
           // in the promise of the estimateSinglePose method, log the end time and TensorFlow.js backend and the pose information
           var toc = new Date().getTime();
-
           console.log(toc - tic, " ms");
           console.log(tf.getBackend());
           console.log(pose);
 
           drawCanvas(pose, videoWidth, videoHeight, canvasRef);
-         });
-        }, 100);
+        });
+      }, 100);
     }
   };
 
@@ -100,6 +97,7 @@ function App() {
 
   };
 
+  // Pose estimation is calculated and displayed on the canvas over the webcam view
   const drawCanvas = (pose, videoWidth, videaoHeight, canvas) => {
   
     const context = canvas.current.getContext("2d");
@@ -112,67 +110,58 @@ function App() {
     // drawBoundingBox(pose["keypoints"], context);
   };
 
+  //? <img src={logo} className="App-logo" alt="logo" /> prevents the Webcam working ?
   return (
     <div className="App">
-      <header className="App-header">
+
+  {/*<img src={logo} className="App-logo" alt="logo" />
+       <WelcomeMessage children={'Welcome to the Virtual Fitness Assistant!'} />
+       <a
+        className="App-link"
+        href="https://www.manning.com/bundles/pose-estimation-with-TensorFlowjs-ser"
+        target="_blank"
+        rel="noopener noreferrer"
+       > 
+         Human Pose Estimation with TensorFlow.js and React
+      </a>
+  */}
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}></Grid>
+      </Grid>
+      <Grid containe spacing={3}>
+        <Grid item xs={12}></Grid>
+      </Grid>
        
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <WelcomeMessage children={'Welcome to the Virtual Fitness Assistant!'} />
-        <a
-          className="App-link"
-          href="https://www.manning.com/bundles/pose-estimation-with-TensorFlowjs-ser"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Human Pose Estimation with TensorFlow.js and React
-        </a>
       
-        <Webcam ref={webcamRef} //ref stores the state so that updating the state doesn’t cause the component to re-render.
-                style={{
-                  position: "absolute",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  left: 0,
-                  right: 0,
-                  textAlign: "center",
-                  zindex: 9,
-                  width: 800,  
-                  height: 600, 
-                }}
-        />
+      {/* <Webcam ref={webcamRef} //ref stores the state so that updating the state doesn’t cause the component to re-render.
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 9,
+              width: 800,  
+              height: 600, 
+            }}
+          
 
-        <canvas ref={canvasRef}
-                style={{
-                position: "absolute",
-                marginLeft: "auto",
-                marginRight: "auto",
-                left: 0,
-                right: 0,
-                textAlign: "center",
-                zindex: 9,
-                width: 800,  
-                height: 600, 
-              }}
-        />
-
-        <button style={{
-          height: "50px",
-          width: "100px",
-          position: "relative",
+       <canvas ref={canvasRef}
+          style={{
+          position: "absolute",
           marginLeft: "auto",
           marginRight: "auto",
-          top: 125,
           left: 0,
           right: 0,
           textAlign: "center",
-          zindex: 9}} 
-          onClick = {handlePoseEstimation}>
-          {isPoseEstimation ? "Stop" : "Start"}
-        </button>
+          zindex: 9,
+          width: 800,  
+          height: 600, 
+          }}
+        /> */}
 
-        
-      </header>
     </div>
   );
 }
