d0fb7f393c2bcf44bb71ac8f7f7c39a1added1bb
Ellie McIntosh <e_paskaleva@hotmail.com>
2021-10-16 16:35:55 -0700

    Setting up the inrastructure and the empty functions for data processing and model training.

---
+++ src/modelTraining.js
@@ -0,0 +1,10 @@
+import * as tf from '@tensorflow/tfjs'
+
+
+function buildModel(numOfFeatures) {
+
+}
+
+export async function runTraining(convertedDatasetTraining, convertedDatasetValidatoin, numOfFeatures) {
+
+};
